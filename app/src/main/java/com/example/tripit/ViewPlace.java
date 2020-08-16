edd4cc3edd1812e6bdd167e94ec79926b31bbc1f
SurekhaChandrarathna <IT19132938>
2020-08-16 10:32:27 +0530

    third commit

---
+++ app/src/main/java/com/example/tripit/ViewPlace.java
@@ -0,0 +1,14 @@
+package com.example.tripit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_place);
+    }
+}
